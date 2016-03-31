8671530fccc4421f52cd1dd52efa167005bb31f1
mrcalvo <michelrc810@gmail.com>
2016-03-31 10:22:57 -0400

    update redirect_uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "6a7124932123d6211b03436883f3ba0b"
   config.secret = "522df354edec31ef840c99c7ddecd2f9"
+  config.redirect_uri = "http://appinfive2.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
